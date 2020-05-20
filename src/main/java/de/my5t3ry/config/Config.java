418b51af5bfff007729d5d7c8042a9f62c22a8d4
my5t3ry <sash@mischkonsum.org>
2020-05-20 18:14:12 +0200

    init

--- src/main/java/de/my5t3ry/config/Config.java
+++ src/main/java/de/my5t3ry/config/Config.java
@@ -1,10 +1,18 @@
 package de.my5t3ry.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /** User: my5t3ry Date: 5/20/20 5:50 AM */
 @Configuration(proxyBeanMethods = false)
 @PropertySource(value = "classpath:application.properties", name = "default")
 @PropertySource(value = "classpath:hibernate.properties", name = "hibenate")
-public class Config {}
+public class Config {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
