c6cfb77c4e177b3ce59c71e50121ab5a8ca5f79f
my5t3ry <sash@mischkonsum.org>
2020-05-20 19:08:58 +0200

    init

--- src/main/java/de/my5t3ry/command/CommandConfig.java
+++ src/main/java/de/my5t3ry/command/CommandConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 /** User: my5t3ry Date: 5/20/20 7:03 PM */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class CommandConfig {
   @Autowired public ListCommand listCommand;
 }
