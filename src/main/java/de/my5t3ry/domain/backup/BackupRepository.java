7b7efb6cbcd1f8dbff2cfb38af0e8fae9f26338a
my5t3ry <sash@mischkonsum.org>
2020-05-26 07:46:38 +0200

    something

--- src/main/java/de/my5t3ry/domain/backup/BackupRepository.java
+++ src/main/java/de/my5t3ry/domain/backup/BackupRepository.java
@@ -2,12 +2,9 @@ package de.my5t3ry.domain.backup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
-import java.util.List;
-
 /**
  * User: my5t3ry Date: 5/19/20 6:26 AM
  */
 public interface BackupRepository extends JpaRepository<Backup, Long> {
-  List<Backup> findBackupByScheduledBefore(final Date current);
+    //  List<Backup> findBackupByScheduledBefore(final Date current);
 }
